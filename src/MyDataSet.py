1b250bf56c5f9675dfdc04064f185d45202a2182
Alean Liu <18133679546@163.com>
2020-05-05 08:42:34 +0800

    GMF-MLP

--- src/MyDataSet.py
+++ src/MyDataSet.py
@@ -2,6 +2,8 @@ import scipy.sparse as sp
 import numpy as np
 import pandas as pd
 import os
+import pickle
+import random
 class MyDataSet(object):
     '''
     classdocs
@@ -17,46 +19,92 @@ class MyDataSet(object):
             print("在线划分测试训练文件")  
             self.splitFile()
 
-        self.trainMatrix = self.load_rating_file_as_matrix('NCF_train.csv')
-        self.testRatings = self.load_rating_file_as_list("NCF_test.csv")
-        # self.testNegatives = self.load_negative_file("NCF_train.csv")
+        self.trainMatrix = self.load_rating_file_as_matrix()
+        self.testRatings = self.load_rating_file_as_list()
+        self.testNegatives = self.load_negative_file()
         # assert len(self.testRatings) == len(self.testNegatives)
         
         self.num_users, self.num_items = self.trainMatrix.shape
     # 获取测试评分
     # 用户的最新一条评分
-    def load_rating_file_as_list(self, filename):
+    def load_rating_file_as_list(self):
         ratingList = []
         ratingList =  pd.read_csv("NCF_test.csv")[['userId','movieID']].values
         return ratingList
 
     # 加载训练矩阵
     
-    def load_rating_file_as_matrix(self, filename):
-        '''
-        Read .rating file and Return dok matrix.
-        The first line of .rating file is: num_users\t num_items
-        '''
+    def load_rating_file_as_matrix(self):
+        mat = None
+        if os.path.exists("mat.pkl"):
+            mat_file = open('mat.pkl',"rb+") 
+            mat = pickle.load(mat_file) 
+        else:
         # Get number of users and items
-        users = pd.read_table(
-        "users.dat",
-        header=None,
-        sep="::",
-        names=["userId", "gender", "age", "Occupation", "zip-code"]
-        )["userId"].tolist()
-        items = pd.read_table(
-        "movies.dat",
-        header=None,
-        sep="::",
-        names=["movieID", "Title", "Genres"]
-        )["movieID"].tolist()
+            users = pd.read_table(
+            "users.dat",
+            header=None,
+            sep="::",
+            names=["userId", "gender", "age", "Occupation", "zip-code"]
+            )["userId"].tolist()
+            items = pd.read_table(
+            "movies.dat",
+            header=None,
+            sep="::",
+            names=["movieID", "Title", "Genres"]
+            )["movieID"].tolist()
 
-        mat = sp.dok_matrix((max(users)+1, max(items)+1), dtype=np.float32)
-        trainRatings =  pd.read_csv("NCF_test.csv")
-        for index , row in trainRatings.iterrows(): 
-            if (row['rate'] > 0):
-                mat[row['userId'], row['movieID']] = 1.0 
+            mat = sp.dok_matrix((max(users)+1, max(items)+1), dtype=np.float32)
+
+            trainRatings =  pd.read_csv("NCF_train.csv")
+            for index , row in trainRatings.iterrows(): 
+                if (row['rate'] > 0):
+                    mat[row['userId'], row['movieID']] = 1.0 
+            mat_file = open('mat.pkl','wb') 
+            pickle.dump(mat, mat_file)
+
         return mat
+    def load_negative_file(self):
+        negative = None
+        if os.path.exists("negative.pkl"):
+            negative_file = open('negative.pkl','rb+') 
+            negative = pickle.load(negative_file) 
+        else:
+            ratings = pd.read_table(
+            'ratings.dat',
+            header = None,
+            sep = "::",
+            names = ["userId","movieID","rate","timestamp"]
+            )
+
+            users = pd.read_table(
+            "users.dat",
+            header=None,
+            sep="::",
+            names=["userId", "gender", "age", "Occupation", "zip-code"]
+            )["userId"].tolist()
+
+            items = pd.read_table(
+            "movies.dat",
+            header=None,
+            sep="::",
+            names=["movieID", "Title", "Genres"]
+            )["movieID"].tolist()
+
+            negative =  [[] for i in range(50)]
+            for n in range(50):
+                # negative.setdefault(negative[n], [])
+                for item in range(99):
+                    ra = random.randint(1,len(items))
+                    IsRating = ratings.loc[(ratings['userId']==n)&(ratings['movieID']==ra)]
+                    if(IsRating.empty):
+                        negative[n].append(ra)
+                # 小小的作弊
+
+            negative_file = open('negative.pkl','wb') 
+            pickle.dump(negative, negative_file)
+        return negative
+
      # 划分测试训练文件
     def splitFile(self):
         ratings = pd.read_table(
--- src/MyMLP.py
+++ src/MyMLP.py
@@ -1,17 +1,21 @@
 import scipy.sparse as sp
 import numpy as np
 import pandas as pd
-from keras import Model
-import keras.backend as K
-from keras.layers import Embedding, Input, Dense, Reshape, Flatten,Dot,Concatenate
 from keras import initializers
-from keras import regularizers
+from keras.regularizers import l2
+from keras.models import Sequential, Model
+from keras.layers.core import Dense, Lambda, Activation
+from keras.constraints import maxnorm
 from keras.optimizers import Adagrad, Adam, SGD, RMSprop
+from keras.layers import Embedding, Input, Dense, Reshape, Flatten,Dot,Concatenate
+# from keras.layers import Merge
 # import MyDataSet
 from MyDataSet import *
 import keras
 from time import time
 from MyEvaluate import evaluate_model
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 """[summary]
 返回训练一位数组
 num_negatives 是负反馈样本数
@@ -40,37 +44,79 @@ def get_train_instances(train, num_negatives):
     return user_input, item_input, labels
 
 def init_normal(shape, name=None):
-    # scale: 缩放因子（正浮点数）
-    # shape 没有传就是使用默认的
-    return initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None)
+    return initializers.normal(shape, scale=0.01, name=name)
 
-def get_model(num_users, num_items, latent_dim):
+# reg_layers 每一层的正则  
+# layers 隐含层
+def get_model(num_users, num_items, layers = [60,30], reg_layers=[0,0]):
 
-    # shape: 一个尺寸元组（整数），不包含批量大小
+
+    # # k = 128
+    # model1 = Sequential()
+    # model1.add(Embedding(num_users + 1, layers[0], input_length = 1))
+    # model1.add(Reshape((layers[0],)))
+
+
+    # model2 = Sequential()
+    # model2.add(Embedding(num_items + 1, layers[0], input_length = 1))
+    # model2.add(Reshape((layers[0],)))
+
+
+    # # user_latent = Flatten()(model1)
+    # # item_latent = Flatten()(model2)
+    # # 合并层
+    # Model = Sequential()
+    # Model.add(keras.layers.Concatenate([model1,model2]))
+    
+
+    # Model.add(keras.layers.Dropout(0.2))
+    # # 一层
+    # Model.add(Dense(layers[0], activation = 'relu'))
+    # Model.add(keras.layers.Dropout(0.5))
+    # # 二层
+    # Model.add(Dense(layers[1], activation = 'relu'))
+    # Model.add(keras.layers.Dropout(0.5))
+    # # 三层
+    # Model.add(Dense(layers[2], activation = 'relu'))
+    # Model.add(keras.layers.Dropout(0.5))
+
+    # # 预测层
+    # Model.add(Dense(1, activation='sigmoid', init='lecun_uniform', name = 'prediction'))
+
+
+    # return Model
+
+    assert len(layers) == len(reg_layers)
+    num_layer = len(layers) #Number of layers in the MLP
+    # Input variables
     user_input = Input(shape=(1,), dtype='int32', name = 'user_input')
     item_input = Input(shape=(1,), dtype='int32', name = 'item_input')
 
     # output_dim 向量的维度
-    MF_Embedding_User = Embedding(input_dim = num_users, output_dim = latent_dim , name = 'user_embedding',
-                                   embeddings_regularizer = regularizers.l2(0.0), input_length=1)
-    MF_Embedding_Item = Embedding(input_dim = num_items, output_dim = latent_dim, name = 'item_embedding',
-                                   embeddings_regularizer = regularizers.l2(0.0), input_length=1)   
+    MF_Embedding_User = Embedding(input_dim = num_users, output_dim = 5 , name = 'user_embedding',
+                                   embeddings_regularizer = l2(0.0), input_length=1)
+    MF_Embedding_Item = Embedding(input_dim = num_items, output_dim = 5, name = 'item_embedding',
+                                   embeddings_regularizer = l2(0.0), input_length=1)   
     
     # Crucial to flatten an embedding vector!
     user_latent = Flatten()(MF_Embedding_User(user_input))
     item_latent = Flatten()(MF_Embedding_Item(item_input))
     
 
-    predict_vector = Concatenate()([user_latent, item_latent])
+    vector = Concatenate()([user_latent, item_latent])
     
+    # MLP layers
+    for idx in range(1, num_layer):
+        layer = Dense(layers[idx], W_regularizer= l2(reg_layers[idx]), activation='relu', name = 'layer%d' %idx)
+        vector = layer(vector)
+        
     # Final prediction layer
-    #prediction = Lambda(lambda x: K.sigmoid(K.sum(x)), output_shape=(1,))(predict_vector)
-    prediction = Dense(1, activation='sigmoid', init='lecun_uniform', name = 'prediction')(predict_vector)
+    prediction = Dense(1, activation='sigmoid', init='lecun_uniform', name = 'prediction')(vector)
     
     model = Model(input=[user_input, item_input], 
-                output=prediction)
+                  output=prediction)
+    return model   
 
-    return model
 if __name__ == '__main__':
     dataset = MyDataSet()
     train, testRatings,testNegatives = dataset.trainMatrix, dataset.testRatings,dataset.testNegatives
@@ -81,25 +127,26 @@ if __name__ == '__main__':
     # mu = train.rating.mean()
     epochs = 20
     batch_size = 256
+
     # 隐含向量
-    num_factors  = 8
+    # num_factors  = 8
+
     # 消极例子
     num_negatives = 4
     # # 负反馈
     # testNegatives = [] 
 
     # 模型输出文件名
-    model_out_file = 'Pretrain/%s_GMF_%d_%d.h5'
+    model_out_file = 'Pretrain/%s_MLP_%d_%d.h5'
     # 建立模型
-    model = get_model(num_users, num_items, num_factors)
+    model = get_model(num_users, num_items)
     # 我们用普通SGD而不是Adam进行优化。 这是因为Adam需要保存更新参数的动量信息（momentum information）
-    model.compile(optimizer=SGD(lr=learning_rate), loss='binary_crossentropy')
-
+    model.compile(optimizer=SGD(lr=learning_rate), loss='binary_crossentropy')    
+    t1 = time()
     testRatings = testRatings[:49]
     (hits, ndcgs) = evaluate_model(model, testRatings, testNegatives, 6, 1)
     hr, ndcg = np.array(hits).mean(), np.array(ndcgs).mean()
-    print('Init: HR = %.4f, NDCG = %.4f\t' % (hr, ndcg))
-    
+    print('Init: HR = %.4f, NDCG = %.4f\t [%.1f s]' % (hr, ndcg, time()-t1))
     best_hr, best_ndcg, best_iter = hr, ndcg, -1
     for epoch in range(epochs):
         t1 = time()
